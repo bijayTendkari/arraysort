31ffd38f9c84a3765dd400964228bc8aec808611
bijay89 <bijaykumarbikibhai@gmail.com>
2024-06-18 17:43:38 +0530

    commit for app123

---
+++ demoOfLog4j/src/main/java/java/log4j/demo/demoOfLog4j/DemoOfLog4jApplication.java
@@ -0,0 +1,13 @@
+package java.log4j.demo.demoOfLog4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOfLog4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOfLog4jApplication.class, args);
+	}
+
+}
---
+++ demoOfLog4j/src/test/java/java/log4j/demo/demoOfLog4j/DemoOfLog4jApplicationTests.java
@@ -0,0 +1,13 @@
+package java.log4j.demo.demoOfLog4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOfLog4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
